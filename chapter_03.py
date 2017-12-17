7716b677197053b099a5bdf4f8fb0cf7ebaec261
gochab <sulichna@gmail.com>
2017-12-17 18:13:43 +0100

    new chapter

---
+++ chapter_03.py
@@ -0,0 +1,17 @@
+
+def addition():
+    first_number = 30
+    second_number = 60
+    print(first_number + second_number)
+addition()
+
+def multiplication():
+    first_number = 50
+    second_number = 20
+    print(first_number * second_number)
+multiplication()
+
+def height():
+    heigh = raw_input("What is you weight?")
+    print ("You mesure " + heigh)
+height()
